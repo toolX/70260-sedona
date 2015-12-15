efd904378135255a1ff48a43d9874b5c0d6b84e2
Philip Shamsiev <ph.shamsiev@mail.ru>
2015-12-15 14:21:01 +0300

    Форма: отправка формы с помощью ajax

--- js/script.js
+++ js/script.js
@@ -38,4 +38,38 @@
             }
         }
     }
+    
+    if (!("FormData" in window)) {
+        return;
+    }
+    
+    var form = document.querySelector("form");
+    var formLink = document.querySelector(".form-button");
+    
+    formLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        
+        var data = new FormData(form);
+        
+        request(data, function(response) {
+            console.log(response);
+        });   
+    });
+        
+    function request(data, fn) {
+        var xhr = new XMLHttpRequest();
+        var time = (new Date()).getTime();
+
+        xhr.open("post", "https://echo.htmlacademy.ru/adaptive?" + time);
+        xhr.addEventListener("readystatechange", function() {
+            if (xhr.readyState == 3) {
+                formLink.popup.classList.add("form-button--blink");
+            }
+            if (xhr.readyState == 4) {
+                fn(xhr.responseText);
+            }   
+        });
+        
+        xhr.send(data);
+    }
 })();
